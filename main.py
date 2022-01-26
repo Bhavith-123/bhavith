b5d8a904a940f834d4fbb027eb52e29f7ff60096
Bhavith-123 <97665580+Bhavith-123@users.noreply.github.com>
2022-01-26 09:27:34 +0530

    Delete main.py

--- main.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route("/")
-def index():
-    return "<html><body><h3 style='color:blue;'><b>WELCOME TO KB WEB SERVICES</b></h3></body></html>"
